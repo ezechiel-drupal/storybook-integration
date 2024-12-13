f659e305c6a32f3db25b399e8d7dbd2f8e9eff84
Zek <johnstone.zek@gmail.com>
2024-12-13 02:18:50 +0100

    SB twig comp made integratable in Drupal

--- web/themes/custom/storybook/vite.config.js
+++ web/themes/custom/storybook/vite.config.js
@@ -1,18 +1,34 @@
 /* eslint-disable */
-
-import { defineConfig } from "vite"
-import yml from '@modyfi/vite-plugin-yaml';
-import twig from 'vite-plugin-twig-drupal';
-import { join } from "node:path"
+import { defineConfig } from "vite";
+import yml from "@modyfi/vite-plugin-yaml";
+import twig from "vite-plugin-twig-drupal";
+import { join } from "node:path";
 export default defineConfig({
+  root: "src",
+  publicDir: "public",
+  build: {
+    emptyOutDir: true,
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        reset: "./src/css/reset.css",
+        styles: "./src/css/styles.css",
+        card: "./src/components/02-molecules/card/card.css",
+      },
+      output: {
+        assetFileNames: "css/[name].css",
+      },
+    },
+    sourcemap: true,
+  },
   plugins: [
     twig({
       namespaces: {
-        components: join(__dirname, "./src/components"),
-        // Other namespaces maybe be added.
+        atoms: join(__dirname, "./src/components/01-atoms"),
+        molecules: join(__dirname, "./src/components/02-molecules"),
       },
     }),
     // Allows Storybook to read data from YAML files.
     yml(),
   ],
-})
+});
